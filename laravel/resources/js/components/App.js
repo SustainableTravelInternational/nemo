25781c2eb97fb12d96533d4da37173539095e89e
Isabella Brookes <github@isabellabrookes.com>
2019-09-22 17:22:43 +0200

    :fire: Removing unnecessary react files from laravel

--- laravel/resources/js/components/App.js
+++ laravel/resources/js/components/App.js
@@ -1,25 +1,7 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-
-import history from '../history';
-
-import Header from './Header';
-import Home from './Home';
-import UserProfile from './users/UserProfile';
+import React from "react";
 
 const App = () => {
-  return (
-    <Router history={history}>
-      <Header />
-      <div className="ui container">
-        <Switch>
-          <Route path="/profile" exact component={UserProfile} />
-          <Route path="/upload" exact component={UserProfile} />
-          <Route path="*" component={Home} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+    return <div></div>;
+};
 
 export default App;
